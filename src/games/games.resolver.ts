27773d7b700bbbbca8a7ed7d4aad86d46806f6d1
Yoha Braga <braga.yoha@gmail.com>
2022-10-11 10:55:31 -0300

    ✨ make service resolver and module to games

--- src/games/games.resolver.ts
+++ src/games/games.resolver.ts
@@ -1,4 +1,43 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver, Query } from '@nestjs/graphql';
+import { CreateGameInput } from './dto/create-game.input';
+import { UpdateGameInput } from './dto/update-game.input';
+import { Game } from './games.entity';
+import { GamesService } from './games.service';
 
-@Resolver()
-export class GamesResolver {}
+@Resolver('Game')
+export class GamesResolver {
+  constructor(private gamesService: GamesService) {}
+
+  @Query(() => [Game])
+  async games(): Promise<Game[]> {
+    const games = await this.gamesService.getAllGames();
+    return games;
+  }
+
+  @Query(() => Game)
+  async game(@Args('id') id: string): Promise<Game> {
+    const game = await this.gamesService.getGameById(id);
+    return game;
+  }
+
+  @Mutation(() => Game)
+  async createGame(@Args('data') data: CreateGameInput): Promise<Game> {
+    const game = await this.gamesService.createGame(data);
+    return game;
+  }
+
+  @Mutation(() => Game)
+  async updateGame(
+    @Args('id') id: string,
+    @Args('data') data: UpdateGameInput,
+  ): Promise<Game> {
+    const game = this.gamesService.updateGame(id, data);
+    return game;
+  }
+
+  @Mutation(() => Boolean)
+  async deleteGame(@Args('id') id: string): Promise<boolean> {
+    const deleted = await this.gamesService.deleteGame(id);
+    return deleted;
+  }
+}
--- src/games/games.service.ts
+++ src/games/games.service.ts
@@ -1,4 +1,50 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateGameInput } from './dto/create-game.input';
+import { UpdateGameInput } from './dto/update-game.input';
+import { Game } from './games.entity';
 
 @Injectable()
-export class GamesService {}
+export class GamesService {
+  constructor(
+    @InjectRepository(Game) private gamesRepository: Repository<Game>,
+  ) {}
+
+  async getAllGames(): Promise<Game[]> {
+    const games = await this.gamesRepository.find();
+    return games;
+  }
+
+  async getGameById(id: string): Promise<Game> {
+    const game = await this.gamesRepository.findOneBy({ id });
+    if (!game) throw new NotFoundException('Game not found');
+    return game;
+  }
+
+  async createGame(data: CreateGameInput): Promise<Game> {
+    const game = this.gamesRepository.create(data);
+    const gameSaved = await this.gamesRepository.save(game);
+    if (!gameSaved)
+      throw new InternalServerErrorException('Fail in create game');
+    return gameSaved;
+  }
+
+  async updateGame(id: string, data: UpdateGameInput): Promise<Game> {
+    const game = await this.getGameById(id);
+    await this.gamesRepository.update(game, { ...data });
+    const gameUpdated = this.gamesRepository.create({ ...game, ...data });
+    return gameUpdated;
+  }
+
+  async deleteGame(id: string): Promise<boolean> {
+    const game = await this.getGameById(id);
+    const deleted = await this.gamesRepository.delete(game);
+    if (deleted) return true;
+    return false;
+  }
+}
